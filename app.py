99239ac984c6cf51fabdae554b3013d83e55e62a
poojaasri-2006 <psri662006@gmail.com>
2026-08-05 08:23:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,17 +2,38 @@ import os
 import uvicorn
 from fastapi import FastAPI
 from langserve import add_routes
-from workflow import rt_app   # import compiled workflow
+from langchain_core.runnables import RunnableLambda
+from pydantic import BaseModel, Field
+from workflow import rt_app
 
-app = FastAPI(title="LangGraph Crew Workflow API")
+app = FastAPI(title="LangGraph Crew API")
 
-# Expose workflow at /crew
+class AgentInput(BaseModel):
+    input: str = Field(description="Enter your message")
+
+def format_input(x):
+    user_input = x["input"] if isinstance(x, dict) else x.input
+    return {
+        "messages": [
+            {
+                "type": "human",
+                "content": user_input
+            }
+        ]
+    }
+
+chain = (
+    RunnableLambda(format_input)
+    | rt_app
+)
+
 add_routes(
     app,
-    rt_app,
+    chain,
     path="/agent",
-    playground_type="chat"
+    playground_type="default"
 )
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
